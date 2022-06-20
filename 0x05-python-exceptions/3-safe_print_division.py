9b3c7c17deeab51150efb75364fd659f3688fcbe
Adewole Michael <michaseyi@gmail.com>
2022-06-20 22:44:08 +0100

    Python exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result: {}", res)
+    return res
