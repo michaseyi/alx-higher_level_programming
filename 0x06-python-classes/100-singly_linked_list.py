f0b24e79a0f9d20f7130dc442ce3bbc67ddd88e2
Adewole Michael <michaseyi@gmail.com>
2022-06-22 00:24:36 +0100

    singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,49 +1,60 @@
 #!/usr/bin/python3
-"""Module Doc"""
+"""Contains a Node class and a SinglyLinkedList class"""
 
 
 class Node:
-    """Class Doc"""
+    """Defines a Node object with a data and next_node
+       attribute
+    """
 
     def __init__(self, data, next_node=None):
-        """Function Doc"""
+        """Initializes a Node object with the data and next_node param"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Function Doc"""
+        """Returns the value of the private data attribute"""
         return self.__data
 
     @property
     def next_node(self):
-        """Function Doc"""
+        """Returns the value of the private next_node attribute"""
         return self.__next_node
 
     @data.setter
     def data(self, value):
-        """Function Doc"""
+        """Sets the value of the private data attribute
+           It will raise a TypeError if value is not an integer
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @next_node.setter
     def next_node(self, value):
-        """Function Doc"""
+        """Sets the value of the private next_node attribute
+           It will raise a TypeError if value is not of type Node or
+           None
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Class Doc"""
+    """Defines the singly linked list data structure"""
 
     def __init__(self):
-        """Function Doc"""
+        """Initializes the singly linked list object and sets the private
+           head attribute to None
+        """
         self.__head = None
 
     def __str__(self):
-        """Function Doc"""
+        """Returns a string representation of the singly linked list data
+           structure
+        """
         ret = []
         current = self.__head
         while current is not None:
@@ -52,7 +63,9 @@ class SinglyLinkedList:
         return "\n".join(ret)
 
     def sorted_insert(self, value):
-        """Function Doc"""
+        """Inserts item into the singly linked list in a sorted manner
+           (increasing order)
+        """
         new_node = Node(value)
         if self.__head is None:
             self.__head = new_node
