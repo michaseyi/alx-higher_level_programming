47a23895cf99f1234ac3db526387037bd450f284
Adewole Michael <michaseyi@gmail.com>
2022-06-22 00:35:41 +0100

    102-sqare

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Containse class Square"""
+"""Contains class Square"""
 
 
 class Square:
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""Module Doc"""
+"""Contains class Square"""
 
 
 class Square:
-    """Class Doc"""
+    """Defines a square"""
 
     def __init__(self, size=0):
-        """Function Doc"""
+        """Initializes the square object with the size param"""
         self.size = size
 
     def area(self):
-        """Function Doc"""
+        """Gets the area of the square object"""
         return self.__size ** 2
 
     @property
     def size(self):
-        """Function Doc"""
+        """Returns the size of the square object"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Function Doc"""
+        """Sets the value of the private size member of the object
+           The value param must be of type int else a TypeError will be
+           raised
+           The value param must be a positive integer else a ValueError will
+           be raised
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -29,25 +34,25 @@ class Square:
             self.__size = value
 
     def __ne__(self, other):
-        """Function Doc"""
+        """Handles != comparison"""
         return self.area() != other.area()
 
     def __eq__(self, other):
-        """Function Doc"""
+        """Handles == comparison"""
         return self.area() == other.area()
 
     def __lt__(self, other):
-        """Function Doc"""
+        """Handles < comparison"""
         return self.area() < other.area()
 
     def __le__(self, other):
-        """Function Doc"""
+        """Handles <= comparison"""
         return self.area() <= other.area()
 
     def __gt__(self, other):
-        """Function Doc"""
+        """Handles > comparison"""
         return self.area() > other.area()
 
     def __ge__(self, other):
-        """Function Doc"""
+        """Handles >= comparison"""
         return self.area() >= other.area()
