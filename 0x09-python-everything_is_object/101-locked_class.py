26b3e1bd18ad20a3a6cfcef2d023f98bc39f86fa
Adewole Michael <michaseyi@gmail.com>
2022-06-28 23:14:12 +0100

    Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,14 +7,14 @@
 class LockedClass(object):
     """
     LockedClass is a class the prevents user from dyamically creating
-    new instanc attribute, except if the new instance attribute is 
+    new instanc attribute, except if the new instance attribute is
     called first_name
     """
 
     def __setattr__(self, name, value):
         """
         __setattr__ overides the default setattr function of this class
-        to prevent the creation of instance attribute except if the 
+        to prevent the creation of instance attribute except if the
         attribute is named first_name
 
         :param self(LockedClass): is the created instace object of this
