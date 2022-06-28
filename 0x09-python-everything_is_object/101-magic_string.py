2b7ec52576a95521fc7b1917507c10e669bd583d
Adewole Michael <michaseyi@gmail.com>
2022-06-28 22:36:14 +0100

    Everything is object

--- 0x09-python-everything_is_object/101-magic_string.py
+++ 0x09-python-everything_is_object/101-magic_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
-    print("BestSchool, "*2+"BestSchool")
-
-
-magic_string()
+    magic_string.i = getattr(magic_string, "i", -1) + 1
+    return "BestSchool, "*magic_string.i+"BestSchool"
+    
