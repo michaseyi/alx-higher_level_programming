c6f0b2938f62c88de8a86f5f89eb46b41b62aa6d
Adewole Michael <michaseyi@gmail.com>
2022-06-28 23:26:04 +0100

    Everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,19 +11,4 @@ class LockedClass(object):
     called first_name
     """
 
-    def __setattr__(self, name, value):
-        """
-        __setattr__ overides the default setattr function of this class
-        to prevent the creation of instance attribute except if the
-        attribute is named first_name
-
-        :param self(LockedClass): is the created instace object of this
-        class
-        :param name(str): is the name of the attribute to be set
-        :param value(any): is the value of the attribute to be set
-        """
-        if name == "first_name":
-            super.__setattr__(self, name, value)
-        else:
-            raise AttributeError(
-                "'LockedClass' object has no attribute '{}'".format(name))
+    __slots__ = ["first_name"]
