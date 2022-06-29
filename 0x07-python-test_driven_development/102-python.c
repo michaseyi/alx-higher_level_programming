8bf1026e70bdb1be4c02534fed12b1d9b7607277
Adewole Michael <michaseyi@gmail.com>
2022-06-29 15:08:21 +0100

    Python unicode object

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,6 +9,8 @@
 
 void print_python_string(PyObject *p)
 {
+	Py_ssize_t len;
+
 	setlocale(LC_ALL, "C.UTF-8");
 	printf("[.] string object info\n");
 	if (strcmp("str", p->ob_type->tp_name))
@@ -20,7 +22,8 @@ void print_python_string(PyObject *p)
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", ((PyASCIIObject *)p)->length);
+	len = ((PyASCIIObject *)p)->length;
+	printf("  length: %ld\n", len);
 	printf("  value: %ls\n",
-		   PyUnicode_AsWideCharString(p, &((PyASCIIObject *)p)->length));
+		   PyUnicode_AsWideCharString(p, &len));
 }
