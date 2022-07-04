617f7335ff0bc44d9dc0d848c42777a2d52d9c12
Adewole Michael <michaseyi@gmail.com>
2022-07-04 21:02:13 +0100

    python inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,4 +11,6 @@ def is_same_class(obj, a_class):
     :a_class (type): input class
     :return (bool)
     """
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    return False
