1ebcdf8572a25881c27eaaee2b0e4ff0429a4556
Adewole Michael <michaseyi@gmail.com>
2022-07-06 00:20:38 +0100

    python input and output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""
+Defines a function that generates the pascal triangle
+"""
+
 
 def pascal_triangle(n):
+    """
+    pascal_triangle generate n number of lines of the pascal
+    triangle
+    """
+    if n <= 0:
+        return []
     res = [[]for i in range(n)]
-    for i in range(len(n)):
-        
-
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    for line in range(1, n+1):
+        c = 1
+        for j in range(1, line+1):
+            res[line - 1].append(c)
+            c = c * (line - j) // j
+    return res
