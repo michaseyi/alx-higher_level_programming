2d660923b2684495cb97a4c6f43129b5946a5bf9
Adewole Michael <michaseyi@gmail.com>
2022-07-06 00:02:18 +0100

    python input and output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 """This script reads stdin line by line and computes metrics"""
 
-line_count = 0
-total_file_size = 0
-t = table = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
-             405: 0, 500: 0}
 
-
-def print_stat():
+def print_stat(table, total_file_size):
     """
     print_stat prints the stat of the the metrics processed after
     reading 10 lines
@@ -21,14 +16,25 @@ def print_stat():
 
 try:
     from sys import stdin
+    line_count = 0
+    total_file_size = 0
+    t = table = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
+                 405: 0, 500: 0}
     for line in stdin:
         if line_count % 10 == 0 and line_count > 0:
-            print_stat()
+            print_stat(table, total_file_size)
         line = line.split()
-        total_file_size += int(line[8])
-        if int(line[7]) in table.keys():
-            table[int(line[7])] += 1
+        try:
+            total_file_size += int(line[8])
+        except (ValueError, IndexError):
+            pass
+        try:
+            if int(line[7]) in table.keys():
+                table[int(line[7])] += 1
+        except (IndexError):
+            pass
         line_count += 1
+    print_stat(table, total_file_size)
 except KeyboardInterrupt:
-    print_stat()
+    print_stat(table, total_file_size)
     raise
