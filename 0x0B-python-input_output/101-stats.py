def0ae3d9c584281f6b38bee01eb3aedad531c41
Adewole Michael <michaseyi@gmail.com>
2022-07-06 00:07:32 +0100

    python input and output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,27 +14,28 @@ def print_stat(table, total_file_size):
             print("{}: {}".format(key, table[key]))
 
 
-try:
-    from sys import stdin
-    line_count = 0
-    total_file_size = 0
-    t = table = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
+if __name__ == "__main__":
+    try:
+        from sys import stdin
+        line_count = 0
+        total_file_size = 0
+        table = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
                  405: 0, 500: 0}
-    for line in stdin:
-        if line_count % 10 == 0 and line_count > 0:
-            print_stat(table, total_file_size)
-        line = line.split()
-        try:
-            total_file_size += int(line[8])
-        except (ValueError, IndexError):
-            pass
-        try:
-            if int(line[7]) in table.keys():
-                table[int(line[7])] += 1
-        except (IndexError):
-            pass
-        line_count += 1
-    print_stat(table, total_file_size)
-except KeyboardInterrupt:
-    print_stat(table, total_file_size)
-    raise
+        for line in stdin:
+            if line_count % 10 == 0 and line_count > 0:
+                print_stat(table, total_file_size)
+            line = line.split()
+            try:
+                total_file_size += int(line[8])
+            except (ValueError, IndexError):
+                pass
+            try:
+                if int(line[7]) in table.keys():
+                    table[int(line[7])] += 1
+            except (IndexError):
+                pass
+            line_count += 1
+        print_stat(table, total_file_size)
+    except KeyboardInterrupt:
+        print_stat(table, total_file_size)
+        raise
