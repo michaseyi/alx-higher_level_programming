6b42036fd594706ecbb9be734420cc1e0752bf84
Adewole Michael <michaseyi@gmail.com>
2022-08-10 23:13:22 +0100

    Added task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,6 +4,6 @@ SELECT id, name
    WHERE cities.state_id  IN (
       SELECT states.id
          FROM states
-         WHERE states.name = 'California';
+         WHERE states.name = 'California'
    )
    ORDER BY cities.id;
