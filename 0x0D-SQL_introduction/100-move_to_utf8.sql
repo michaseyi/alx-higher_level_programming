9f7b36d48abafe190e452dc88e6d107d5e703bcd
Adewole Michael <michaseyi@gmail.com>
2022-08-10 07:40:28 +0100

    Added task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- convert a database to utf8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CONVERT TO  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
