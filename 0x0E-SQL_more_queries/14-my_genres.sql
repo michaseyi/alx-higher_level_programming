29a595c75baf6097daebf5f4ef6b917bd11539cc
Adewole Michael <michaseyi@gmail.com>
2022-08-11 00:11:55 +0100

    Added task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,10 @@
 -- lists all genres of the show "Dexter"
 
-SELECT name 
-   FROM tv_genres
-   WHERE id IN (
-      SELECT genre_id
-         FROM tv_shows t
-         JOIN tv_show_genres tg
-         ON t.id = tg.show_id
-      WHERE t.title = "Dexter"
-   )
-   ORDER BY name;
+SELECT g.name as name
+   FROM tv_shows t
+   JOIN tv_show_genres tg
+   ON t.id = tg.show_id
+   JOIN tv_genres g
+   ON tg.genre_id = g.id
+   WHERE t.title = "Dexter"
+   ORDER BY g.name;
