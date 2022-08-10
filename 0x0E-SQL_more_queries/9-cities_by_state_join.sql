2f24377c981a003125070937948ebfad79afdced
Adewole Michael <michaseyi@gmail.com>
2022-08-10 23:20:03 +0100

    Added task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,4 +3,5 @@
 SELECT cities.id as id, cities.name as name, states.name as name 
    FROM states
    INNER JOIN cities
-   ON states.id = cities.state_id;
+   ON states.id = cities.state_id
+   ORDER BY cities.id;
