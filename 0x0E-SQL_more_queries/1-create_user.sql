642bea4c699dabc2adb4200f8e5945fa3cd52fe4
Adewole Michael <michaseyi@gmail.com>
2022-08-10 22:37:09 +0100

    Added task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creates a user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED WITH 'authentication_plugin' BY 'user_0d_1_pwd'
