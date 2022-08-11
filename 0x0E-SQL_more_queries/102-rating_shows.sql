66485ce87070f322e94b008aa22380742b5ffc4b
Adewole Michael <michaseyi@gmail.com>
2022-08-11 01:16:15 +0100

    Added task 102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+
+SELECT t.title, SUM(r.rate) as rating
+   FROM tv_shows t
+   JOIN tv_show_ratings r
+   ON t.id = r.show_id
+   GROUP BY t.title
+   ORDER BY rating DESC;
