e0233c928b396ea468ae08ebe425c97a3c190744
Adewole Michael <michaseyi@gmail.com>
2022-09-05 21:35:38 +0100

    Uploaded task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-if (process.argv.slice(1, -1).length > 0) {
+if (process.argv.length > 2) {
+  console.log('Arguments found');
+} else if (process.argv.length > 1) {
   console.log('Argument found');
 } else {
   console.log('No argument');
