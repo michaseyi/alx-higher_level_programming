0d96a29b81002d0e5dca25caee4defd2bc10dc5d
Adewole Michael <michaseyi@gmail.com>
2022-09-06 21:42:22 +0100

    Added task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const sources = [fs.createReadStream(process.argv[2]), fs.createReadStream(process.argv[3])];
+const dest = fs.createWriteStream(process.argv[4]);
+
+sources.forEach((source) => {
+  source.pipe(dest);
+});
