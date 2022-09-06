4e55f05b4f868424818afa2fc83122c7d9f030b6
Adewole Michael <michaseyi@gmail.com>
2022-09-06 21:16:17 +0100

    Added task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  return function (number) {
+    if (number === 0) return '0';
+
+    function convertNumber (number, convertedNumber, alphabets) {
+      if (number === 0) return convertedNumber;
+
+      if (number % base < 10) {
+        return convertNumber(Math.floor(number / base), String(number % base) + convertedNumber, alphabets);
+      }
+      return convertNumber(Math.floor(number / base), alphabets[(number % base) - 10] + convertedNumber, alphabets);
+    }
+
+    return convertNumber(number, '', 'abcdef');
+  };
+};
