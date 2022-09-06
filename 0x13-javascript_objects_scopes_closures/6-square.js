48418d59537e6b6b945d6bf160316de8ac9ef529
Adewole Michael <michaseyi@gmail.com>
2022-09-06 19:53:57 +0100

    Added task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const PrevSquare = require('./5-square');
+
+module.exports = class Square extends PrevSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
