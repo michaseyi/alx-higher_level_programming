b31f80149b6fe54821a9384e35a28f3f451f7495
Adewole Michael <michaseyi@gmail.com>
2022-09-09 19:54:01 +0100

    Added task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module contains the class definition of a State and
+an instance of Base = declarative_base()"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine
+import sys
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents the states table. Define objects that map to
+    rows in the states table
+    """
+
+    __tablename__ = "states"
+
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(url)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
