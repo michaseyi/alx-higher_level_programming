773bc847d94b5edbd025146833954808b8441c0c
Adewole Michael <michaseyi@gmail.com>
2022-09-09 20:25:24 +0100

    Added task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains the class definition of a State and
+an instance of Base = declarative_base()"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(url)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    for state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(state)
+    session.commit()
