c804d854243c2c2501e6e48e6f1b920a143886ef
Adewole Michael <michaseyi@gmail.com>
2022-09-22 23:05:10 +0100

    Added task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # displays all http methods the server will accept
-curl -s "$1" | grep "Allow:" | cut --delim=' ' --field=2-
+curl -si "$1" | grep "Allow:" | cut --delim=' ' --field=2-
 
