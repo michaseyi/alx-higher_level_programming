caa5a1e6ea27e4f081ca4b15b94d52dff11d547a
Adewole Michael <michaseyi@gmail.com>
2022-09-24 00:14:33 +0100

    Added task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,11 +13,10 @@ if __name__ == "__main__":
     response = requests.get(url, params=params)
     try:
         commits = response.json()
-        commits.sort(key=lambda x: x["commit"]["author"]["date"], reverse=True)
         for commit in commits[:10]:
             sha = commit["sha"]
             author = commit["commit"]["author"]["name"]
             date = commit["commit"]["author"]["date"]
             print("{}: {}".format(sha, author))
-    except ValueError:
+    except BaseException:
         ...
