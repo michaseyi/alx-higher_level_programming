c2d9c72d765bd96d05ca22e5c6cb60d46e53bb55
Adewole Michael <michaseyi@gmail.com>
2022-09-23 10:19:44 +0100

    Added task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,23 +3,24 @@
 
 
 def find_peak(list_of_integers):
+    """
+    find_peak finds a peak number from a list of integers
 
-    def __find_peak(l, start, end):
-
+    :param list_of_integers(list[int]): is the list to retrieve peak from
+    :return: is the peak
+    """
+    def __find_peak(vals, start, end):
+        """
+        __find_peak performs binary search to find peak value
+        """
+        if len(vals) < 1:
+            return None
         mid = start + (end - start) // 2
-
-        if (mid <= 0 or l[mid - 1] <= l[mid]) and (mid >= len(l) - 1 or l[mid + 1] <= l[mid]):
-            return mid
-        elif (mid > 0 and l[mid - 1] > l[mid]):
-            return __find_peak(l, end, mid - 1)
+        if (mid <= 0 or vals[mid - 1] <= vals[mid]) and (
+                mid >= len(vals) - 1 or vals[mid + 1] <= vals[mid]):
+            return vals[mid]
+        elif (mid > 0 and vals[mid - 1] > vals[mid]):
+            return __find_peak(vals, start, mid-1)
         else:
-            return __find_peak(l, mid + 1, start)
+            return __find_peak(vals, mid+1, end)
     return __find_peak(list_of_integers, 0, len(list_of_integers)-1)
-
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
