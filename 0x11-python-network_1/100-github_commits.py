407936552fd9d69150611433d6bc3c2385cdfd8c
Adewole Michael <michaseyi@gmail.com>
2022-09-23 23:52:21 +0100

    Added task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script fetches 10 commits from the repository name and owner
+name provided and sortes them from recent to oldest"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1],
+        sys.argv[2]
+    )
+    params = {"per_page": 10}
+    response = requests.get(url, params=params)
+    try:
+        for commit in response.json():
+            sha = commit["sha"]
+            author = commit["commit"]["author"]["name"]
+            date = commit["commit"]["author"]["date"]
+            print("{}: {}".format(sha, author))
+    except ValueError:
+        ...
