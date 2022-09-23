f5a9261d390b862ac4bbe146e95115ac7605dada
Adewole Michael <michaseyi@gmail.com>
2022-09-23 21:49:41 +0100

    Updated task 0 and 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         data = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(data)))
-        print("    - content: {}".format(data))
-        print("    - utf8 content: {}".format(data.decode('utf-8')))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     data = response.text
     print("Body response:")
-    print("    - type: {}".format(type(data)))
-    print("    - content: {}".format(data))
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
