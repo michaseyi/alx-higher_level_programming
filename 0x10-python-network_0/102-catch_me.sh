b6dca2b1c164ac95834c9c3b1cdcca54b927c454
Adewole Michael <michaseyi@gmail.com>
2022-09-24 11:14:50 +0100

    Updated task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # causes server to respond with "You got me!"
-curl -s -L -H 'Origin: School' -d "user_id=98" -X PUT 0.0.0.0:5000/catch_me
+curl -s -L -H 'Origin: HolbertonSchool' -d "user_id=98" -X PUT 0.0.0.0:5000/catch_me
