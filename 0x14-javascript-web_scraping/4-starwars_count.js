4fc49d85162bbda16d202adca07d86eead7ee893
Adewole Michael <michaseyi@gmail.com>
2022-10-18 20:06:18 +0100

    Added task 4 web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,14 @@ request.get(process.argv[2]).on('response', (res) => {
     const body = JSON.parse(Buffer.concat(chunks));
     const count = body.results.reduce((total, curr) => {
       const characters = curr.characters;
-      if (characters.includes('https://swapi-api.hbtn.io/api/people/18/')) return total + 1;
+      // if (characters.includes('https://swapi-api.hbtn.io/api/people/18/')) return total + 1;
+      if (characters.some((curr) => {
+        const paths = curr.split('/');
+        return +paths[paths.length - 2] === 18;
+      }
+      )) {
+        return total + 1;
+      }
       return total;
     }, 0);
     console.log(count);
